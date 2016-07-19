c8de74ce12844c7da86ced432bff888799de8590
jmkiley <jordankiley@gmail.com>
2016-07-19 11:40:38 -0400

    Changes to be committed: modified:   MapProj.xcodeproj/project.pbxproj modified:   MapProj.xcworkspace/xcuserdata/Jordan.xcuserdatad/UserInterfaceState.xcuserstate modified:   MapProj/Base.lproj/Main.storyboard new file:   MapProj/redpanda3d.png modified:   TakeAPhoto.swift modified:   MapProj.xcworkspace/xcuserdata/Jordan.xcuserdatad/UserInterfaceState.xcuserstate

--- TakeAPhoto.swift
+++ TakeAPhoto.swift
@@ -12,13 +12,72 @@ import AVFoundation
 
 class TakeAPhoto : UIViewController {
     let stillImageOutput = AVCaptureStillImageOutput()
-
+    var preview =  AVCaptureVideoPreviewLayer()
+    var imageToSave = UIImage()
     @IBOutlet weak var redPanda: UIImageView!
+    @IBOutlet weak var picture: UIImageView!
 //    @IBOutlet weak var cameraView: UIImageView!
     @IBOutlet weak var save: UIButton!
 //    @IBOutlet weak var takePicture: UIButton!
     @IBOutlet weak var cancel: UIButton!
     
+//     Saves the camera's output
+    @IBAction func savePicture(sender: AnyObject) {
+//        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
+//            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
+//                (imageDataSampleBuffer, error) -> Void in
+//                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+//                let imageToSave = (UIImage(data: imageData))
+//                
+//                UIImageWriteToSavedPhotosAlbum(imageToSave!, nil, nil, nil)
+//
+//            }
+//        }
+//        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
+//            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
+//                (imageDataSampleBuffer, error) -> Void in
+//                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+//                self.imageToSave = (UIImage(data: imageData))!
+//            }
+//        }
+//        self.picture.image = self.imageToSave
+//        
+//        
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("hi")
+//        var imageToSave = UIImage()
+        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
+            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
+                (imageDataSampleBuffer, error) -> Void in
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
+                self.imageToSave = (UIImage(data: imageData))!
+            }
+        }
+        self.picture.image = self.imageToSave
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.picture.image = self.imageToSave
+        let layer = UIApplication.sharedApplication().keyWindow!.layer
+        self.save.hidden = true
+        self.cancel.hidden = true
+        let scale = UIScreen.mainScreen().scale
+        
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+
+        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+
+        UIGraphicsEndImageContext()
+        //Save it to the camera roll
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.save.hidden = false
+        self.cancel.hidden = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCameraSession()
@@ -26,24 +85,11 @@ class TakeAPhoto : UIViewController {
         cameraSession.startRunning()
         self.view.bringSubviewToFront(redPanda)
         self.view.bringSubviewToFront(cancel)
-//        self.view.bringSubviewToFront(takePicture)
+        //        self.view.bringSubviewToFront(takePicture)
         self.view.bringSubviewToFront(save)
-    }
-    
-    // Saves the camera's output
-    @IBAction func savePicture(sender: AnyObject) {
-        if let videoConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo) {
-            stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection) {
-                (imageDataSampleBuffer, error) -> Void in
-                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)
-                UIImageWriteToSavedPhotosAlbum(UIImage(data: imageData)!, nil, nil, nil)
-            }
-        }
-    }
-    
-    func viewDidAppear() {
-
         
+    }
+    func viewDidAppear() {
     }
     
     lazy var cameraSession: AVCaptureSession = {
@@ -55,12 +101,25 @@ class TakeAPhoto : UIViewController {
     
     //Makes the video appear
     lazy var previewLayer: AVCaptureVideoPreviewLayer = {
-        let preview =  AVCaptureVideoPreviewLayer(session: self.cameraSession)
-        preview.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        preview.position = CGPoint(x: CGRectGetMidX(self.view.bounds), y: CGRectGetMidY(self.view.bounds))
-        preview.videoGravity = AVLayerVideoGravityResize
-        self.view.layer.insertSublayer(preview, atIndex: 0)
-        return preview
+        self.preview =  AVCaptureVideoPreviewLayer(session: self.cameraSession)
+        self.preview.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        self.preview.position = CGPoint(x: CGRectGetMidX(self.view.bounds), y: CGRectGetMidY(self.view.bounds))
+        self.preview.videoGravity = AVLayerVideoGravityResize
+    
+//        let path = NSBundle.mainBundle().pathForResource("redpanda3d", ofType: "png")
+//        let redPandaImage = UIImage(contentsOfFile: path!)!
+//
+        let redPandaLayer = CALayer(layer: self.redPanda)
+        
+        self.preview.addSublayer(redPandaLayer)
+        self.preview.insertSublayer(redPandaLayer, above: self.preview)
+//        redPandaLayer.bounds = CGRect(x: 50, y: 50, width: 91, height: 86)
+//        redPandaLayer.position = CGPoint(x: CGRectGetMidX(redPandaLayer.bounds), y: CGRectGetMidY(redPandaLayer.bounds))
+//        
+//        let redPandaAnimation = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: redPandaLayer, inLayer: self.preview)
+//        self.view.layer.insertSublayer(preview, atIndex: 0)
+        
+        return self.preview
     }()
     
     func setupCameraSession() {
@@ -91,10 +150,14 @@ class TakeAPhoto : UIViewController {
         //Makes camera session's output a StillImage
         if cameraSession.canAddOutput(stillImageOutput) {
             cameraSession.addOutput(stillImageOutput)
+            
         }
 
     }
-    
+//    func getImageSize(image : UIImage) -> CGSize {
+//        var imagex = image.size
+//        
+//    }
     override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
 }
